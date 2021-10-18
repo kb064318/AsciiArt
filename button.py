00c5483179675d92507f5b80ed2f6ed39ca4cdf2
Kyle Bryant <kb064318@ohio.edu>
2021-10-18 15:16:14 -0400

    Fixed input image loading, added RGB value print function (currently not fully functional)

--- button.py
+++ button.py
@@ -5,6 +5,7 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang.builder import Builder
 from kivy.core.image import Image
+from kivy.config import Config
 from plyer import filechooser
 
 class LayoutScreen(GridLayout):
@@ -23,14 +24,23 @@ class LayoutScreen(GridLayout):
         self.add_widget(Label())
         self.add_widget(Label(text="image6"))
 
+# Returns pixel RGB value of an image at specified coordinates
+def get_pixel(img, x, y):
+    color = img.read_pixel(x, y)
+    return color[0] * 255, color[1] * 255, color[2] * 255
+
 class SelectButton(Button):
     def choose_file(self, val):
-        path = filechooser.open_file(title="Select a Photo", 
+        # Opens file chooser
+        filePath = filechooser.open_file(title="Select a Photo", 
                              filters=[("Photos", "*.png")])
-        print(path)
-        m = Image.load(path, keep_data=True)
-        color = m.read_pixel(x, y)
-        return color[0] * 255
+        # Declares img as the image picked
+        print(*filePath)
+        img = Image.load(*filePath, keep_data=True)
+        # Prints all pixel RGB values in file
+        for i in range(0, img.size[0]):
+            for j in range (0, img.size[1]):
+                print(i, j, get_pixel(img, i, j))
 
 class ButtonMain(App): 
     def build(self):
