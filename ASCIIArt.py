622cb98fa477052d3375d79ee5137a067b2adaef
Cory Zink <cz109918@ohio.edu>
2021-11-02 16:33:20 -0400

    rebuilt LayoutScreen with kv file

--- ASCIIArt.py
+++ ASCIIArt.py
@@ -1,8 +1,7 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.lang.builder import Builder
 from kivy.core.image import Image
 from kivy.config import Config
@@ -11,21 +10,12 @@ from pixelConversionAlgorithm import algorithm, text_to_image
 import os
 
 # Front-End Design. - Lets us organize the GUI.
-class LayoutScreen(GridLayout):
+class LayoutScreen(BoxLayout):
     def __init__(self, **kwargs):
         super(LayoutScreen, self).__init__(**kwargs)
-        self.cols = 3                   # Creates three columns for the widgets to occupy.
-        self.add_widget(Label(text="")) 
-        self.add_widget(Label())
-        self.add_widget(Label(text=""))
-        self.add_widget(Label(text=""))
-        btn = SelectButton()
-        btn.bind(on_press=btn.choose_file)
-        self.add_widget(btn)
-        self.add_widget(Label(text=""))
-        self.add_widget(Label(text=""))
-        self.add_widget(Label())
-        self.add_widget(Label(text=""))
+        # finds button object and binds the choose_file function to the on_press event
+        btn = self.ids.selectbtn # selectbtn is the id of the button object in the .kv file
+        btn.bind(on_press=SelectButton.choose_file)
 
 # Returns pixel RGB value of an image at specified coordinates
 def get_pixel(img, x, y):
@@ -39,7 +29,7 @@ def get_pixel_gray(img, x, y):
 
 # Back-end for file browse button.
 class SelectButton(Button):
-    def choose_file(self, val):
+    def choose_file(self):
         # Opens file chooser
         file_path = filechooser.open_file(title="Select a Photo to convert", 
                              filters=[("Photos", "*.jpg")])
@@ -61,7 +51,6 @@ class SelectButton(Button):
 
 class ASCIIArt(App): 
     def build(self):
-        #Builder.load_file('ASCIIArt.kv')
         return LayoutScreen()
 
 ASCIIArt().run()
