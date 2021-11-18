97b3de0610b815a2ca1f5f3a7bce067ffd9f302e
Kyle Bryant <kb064318@ohio.edu>
2021-11-18 02:46:01 -0500

    Corrected generated image width and height. Added font with equal width and height for each character.

--- pixelConversionAlgorithm.py
+++ pixelConversionAlgorithm.py
@@ -5,16 +5,20 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang.builder import Builder
 from PIL import Image, ImageDraw, ImageFont
+import os
 
 def algorithm(path):
     im = Image.open(path,  'r')
     width, height = im.size
     pix_val = list(im.getdata())
+    ascii_list = []
     ascii_string = ""
     print("Converting photo to ascii... (May take a while)")
     for x in range(len(pix_val)):
         if(x % width == 0):
-            ascii_string += "\n"
+            # Adds each string as a separate element in ascii_list. Used to set the space between each line of text in text_to_image()
+            ascii_list.append(ascii_string)
+            ascii_string = ""
         if (pix_val[x][0] < 17):
             #black
             ascii_string += "A"
@@ -62,16 +66,15 @@ def algorithm(path):
             ascii_string += "O"
         else:
             #white
-            # This amount of spaces is required to make the photo look normal
             ascii_string += " "
     im.close()
-    return ascii_string
+    return ascii_list
         
 '''
 Converts text to png file and saves it to photo_name. Having this in the same
 file as a function that uses kivy.core.image causes namespace problems.
 '''
-def text_to_image(path, ascii_string, photo_name):
+def text_to_image(path, ascii_list, photo_name):
     print("Saving image...")
     
     # Gets width and height of image
@@ -79,10 +82,17 @@ def text_to_image(path, ascii_string, photo_name):
     width, height = im.size
     im.close()
 
+    # Gets font path and size
+    current_path = os.path.dirname(__file__)
+    font_path = os.path.join(current_path, 'Font/DeferralSquare.ttf')
+    font_size = 8 # Currently, only size 8 font works correctly
+    
     # Generates an image based on the contents of ascii_string and saves it
-    ascii_font = ImageFont.truetype('cour.ttf', 10) # courier new is the base font
-    ascii_image = Image.new(mode="RGB", size=(width*9, height*12), color=(255, 255, 255))
+    ascii_font = ImageFont.truetype(font_path, font_size) # courier new is the base font
+    ascii_image = Image.new(mode="RGB", size=(width*font_size, height*font_size), color=(255, 255, 255))
     draw_ascii = ImageDraw.Draw(ascii_image)
-    draw_ascii.text(xy=(0, 0), text=ascii_string, font=ascii_font, fill=(0, 0, 0))
+    for x in range(len(ascii_list)): # Draws every ascii_string in ascii_list, with a y position that makes each character equally spaced
+        draw_ascii.text(xy=(0, x*font_size), text=ascii_list[x], font=ascii_font, fill=(0, 0, 0))
+
     ascii_image.save(photo_name)
     return ascii_image
